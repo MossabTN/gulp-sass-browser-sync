f22a1467e3da4eeab67f6162be9593ffac82f939
SkaZZ7 <mahat.mossab@gmail.com>
2016-12-26 02:02:33 +0100

    first

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoPrefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync').create();
+
+gulp.task('styles', function() {
+    gulp.src('./css/style.scss')
+        .pipe(sass())
+        .pipe(autoPrefixer())
+        .pipe(gulp.dest('./css'))
+        .pipe(browserSync.reload({stream:true}))
+});
+
+
+gulp.task('server', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
+
+gulp.task('watch', function () {
+    gulp.watch('./css/*.scss', ['styles']);
+    gulp.watch(['./*.html','./example/*.html']).on('change',browserSync.reload);
+});
+
+gulp.task('default',['styles','watch','server']);
